dbc2e1d3c0bfb20be3722621199874443220ad27
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-05-23 18:28:24 +0700

    Update edit.blade.php

--- resources/views/subjects/edit.blade.php
+++ resources/views/subjects/edit.blade.php
@@ -12,6 +12,10 @@
             <label for="name" class="form-label">Tên Môn Học</label>
             <input type="text" class="form-control" id="name" name="name" value="{{ $subject->name }}" required>
         </div>
+        <div class="mb-3">
+            <label for="credit" class="form-label">Số tín chỉ</label>
+            <input type="number" class="form-control" name="credit" id="credit" value="{{ old('credit', $subject->credit ?? '') }}" required min="1">
+        </div>
         <button type="submit" class="btn btn-primary">Cập nhật môn học</button>
     </form>
 @endsection
