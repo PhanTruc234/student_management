b0240a0e076351913d1dc285847054e076b94398
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-05-23 18:31:32 +0700

    Update 2025_05_23_104004_add_credit_to_subjects_table.php

--- database/migrations/2025_05_23_104004_add_credit_to_subjects_table.php
+++ database/migrations/2025_05_23_104004_add_credit_to_subjects_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('subjects', function (Blueprint $table) {
-            $table->integer('credit')->default(1); // số tín chỉ, mặc định là 1
+            $table->integer('credit')->default(1); 
         });
     }
 
