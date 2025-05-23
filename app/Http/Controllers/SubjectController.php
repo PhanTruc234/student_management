54622f62f1e959d9ec373686b142b84770e2e521
PhanTruc234 <166579889+PhanTruc234@users.noreply.github.com>
2025-05-23 18:29:39 +0700

    Update SubjectController.php

--- app/Http/Controllers/SubjectController.php
+++ app/Http/Controllers/SubjectController.php
@@ -32,11 +32,13 @@ class SubjectController extends Controller
         $request->validate([
             'name' => 'required|string|max:255|unique:subjects,name',
             // bắt buộc, là chuỗi, tối đa 255 ký tự, và không trùng tên môn học đã có.
+            'credit' => 'required|integer|min:1'
         ]);
         $subjectCode = 'PKA' . str_pad(Subject::count() + 1, 3, '0', STR_PAD_LEFT);
         Subject::create([
             'code' => $subjectCode,
             'name' => $request->name,
+            'credit' => $request->credit,
         ]);
 
         return redirect()->route('subjects.index')->with('success', 'Môn học đã được thêm');
@@ -51,10 +53,12 @@ class SubjectController extends Controller
     {
         $request->validate([
             'name' => 'required|string|max:255|unique:subjects,name,' . $subject->id,
+            'credit' => 'required|integer|min:1'
         ]);
 
         $subject->update([
             'name' => $request->name,
+            'credit' => $request->credit,
         ]);
 
         return redirect()->route('subjects.index')->with('success', 'Môn học đã được cập nhật');
